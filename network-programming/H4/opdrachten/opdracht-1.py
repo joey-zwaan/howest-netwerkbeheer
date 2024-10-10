ca4808855a64994a18069db3669973d69ff0dbdb
Joey Zwaan <joey.zwaan@student.howest.be>
2024-10-10 11:09:43 +0200

    opdracht-1 gemaakt

--- network-programming/H4/opdrachten/opdracht-1.py
+++ network-programming/H4/opdrachten/opdracht-1.py
@@ -6,18 +6,18 @@ import time
 #Na 0 volgt "lift-off"
 countdown = int(input("From what number would you like to countdown from?"))
 
+
 while countdown != 0:
     if countdown > 60:
         countdown -= 10
         time.sleep(10)
         print(countdown)
 
-    elif countdown == 60 or  countdown <60 :
+    elif countdown == 60 or countdown <60 :
         countdown -= 1
         time.sleep(1)
         print(countdown)
 
-else :
-    print("lift off!")
+print("lift off!")
 
 
